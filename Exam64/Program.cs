01bbf6ef165273ae59b5776eba0278df8f74e479
Nikolay <labazopelastra@gmail.com>
2022-12-26 02:51:09 +0700

    Add exam64

---
+++ Exam64/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все четные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+
+System.Console.Write("Введите число: ");
+int N=int.Parse(Console.ReadLine()!);
+
+string EvenNumbers(int start, int end)
+{
+    while (start % 2 != 0)
+    {
+        start--;
+    }
+    if (end == start)
+    {
+        return end.ToString();
+    }
+    return (start + " " + EvenNumbers(start - 2, end));
+}
+
+Console.WriteLine(EvenNumbers(N, 0));
